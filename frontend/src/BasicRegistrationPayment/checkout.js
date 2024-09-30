c1d437e5a2b626dcdfd6d01827960258dfc37eef
Rekha <136164355+rekha-beniwal@users.noreply.github.com>
2024-09-30 15:30:46 +0530

    Update checkout.js

--- frontend/src/BasicRegistrationPayment/checkout.js
+++ frontend/src/BasicRegistrationPayment/checkout.js
@@ -1,48 +1,3 @@
-// import axios from "axios";
-// import {BASE_URL} from "../BaseUrl"
-// import img from "../Images/Logo.png"
-// export  const checkoutHandler = async (amount,userInfo,formData,callback) => {
-    
-  
-//     try {
-//       const { data: { key } } = await axios.get(`${BASE_URL}/api/getkey`);
-//       const { data: { order } } = await axios.post(`${BASE_URL}/api/checkout`, { amount });
-  
-     
-  
-//       const options = {
-//         key,
-//         amount: order.amount,
-//         currency: "INR",
-//         name: "Pyrexia",
-//         description: "Aiims Rishikesh Fest",
-//         image: {img},
-//         order_id: order.id,
-//         name: userInfo.name,
-//         email: userInfo.email,
-//         mobile:formData.mobile,
-//         tickets:formData.tickets,
-//         callback_url: `${callback}`,
-//         prefill: {
-//           name: userInfo.name,
-//           email: userInfo.email,
-//           contact: "",
-//         },
-//         notes: {
-//           address: "Aiims Rishikesh",
-//         },
-//         theme: {
-//           color: "#001f13",
-//         },
-//       };
-  
-//       const razor = new window.Razorpay(options);
-//       razor.open();
-//     } catch (error) {
-//       console.error("Error in checkoutHandler:", error);
-//     }
-//   };
-
 import axios from "axios";
 import { BASE_URL } from "../BaseUrl";
 import img from "../Images/Logo.png";
@@ -79,6 +34,7 @@ export const checkoutHandler = async (amount, userInfo, formData, callback,navig
           razorpay_order_id: response.razorpay_order_id,
           razorpay_payment_id: response.razorpay_payment_id,
           razorpay_signature: response.razorpay_signature,
+         amount:amount,
           name: userInfo.name,
           email: userInfo.email,
           mobile: formData.mobile,
