a312e084ff6c669e723e128505443ffab338d735
Rekha <136164355+rekha-beniwal@users.noreply.github.com>
2024-09-30 22:03:46 +0530

    Update BasicRegistration.js

--- frontend/src/components/BasicRegistration.js
+++ frontend/src/components/BasicRegistration.js
@@ -32,7 +32,7 @@ const BasicRegistration = () => {
           <div className="flex justify-center items-center mt-10 p-4">
             <button
               className="bg-[#ebe6d0] hover:bg-gradient-to-t from-blue-500 to-white text-black px-6 py-2.5 rounded-lg font-bold text-sm border-black hover:bg-[#d9d2b8] transition duration-300"
-              onClick={() => {navigate("/basic-registration-form")}}
+              onClick={() => window.location.href = "https://forms.gle/PRjwsH44sZiHBaUr5"}
             >
               Register Now 
             </button>
