ad39c30c809b7bb84ba141c95122787a1deee90f
Rekha <136164355+rekha-beniwal@users.noreply.github.com>
2024-10-01 16:40:57 +0530

    Update Soon.js

--- frontend/src/BasicRegistrationPayment/Soon.js
+++ frontend/src/BasicRegistrationPayment/Soon.js
@@ -4,7 +4,7 @@ const Soon = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-white text-white pt-24">
             <h1 className="text-4xl animate-pulse font-semibold text-[#001f3f] font-sans-serif poppins">
-                Registration will start soon
+                Registration opening again shortly.
             </h1>
          
         </div>
